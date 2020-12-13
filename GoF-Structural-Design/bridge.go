657d018143c0ebe04f34f7a0a7728e9da348b154
ncolesummers <nsummers72@gmail.com>
2020-12-13 11:32:40 -0800

    fix last commit

--- GoF-Structural-Design/bridge.go
+++ GoF-Structural-Design/bridge.go
@@ -8,25 +8,23 @@ type IDrawShape interface {
 
 type DrawShape struct{}
 
-// DrawShape struct has method draw Shape with float x and y coordinates
-
 func (drawShape DrawShape) drawShape(x [5]float32, y [5]float32) {
 	fmt.Println("Drawing Shape")
 }
 
 type IContour interface {
-	drawContour(x[5] float32, y[5] float32)
+	drawContour(x [5]float32, y [5]float32)
 	resizeByFactor(factor int)
 }
 
 type DrawContour struct {
-	x[5] float32
-	y[5] float32
-	shape DrawShape
+	x      [5]float32
+	y      [5]float32
+	shape  DrawShape
 	factor int
 }
 
-func (contour DrawContour) drawContour(x[5] float32, y[5] float32) {
+func (contour DrawContour) drawContour(x [5]float32, y [5]float32) {
 	fmt.Println("Drawing Contour")
 	contour.shape.drawShape(contour.x, contour.y)
 }
@@ -37,10 +35,10 @@ func (contour DrawContour) resizeByFactor(factor int) {
 
 func main() {
 	var (
-		x = [5]float32{1,2,3,4,5}
-		y = [5]float32{1,2,3,4,5}
-		contour IContour = DrawContour{x,y, DrawShape{},2}
+		x                = [5]float32{1, 2, 3, 4, 5}
+		y                = [5]float32{1, 2, 3, 4, 5}
+		contour IContour = DrawContour{x, y, DrawShape{}, 2}
 	)
-	contour.drawContour(x,y)
+	contour.drawContour(x, y)
 	contour.resizeByFactor(2)
 }
